253c22e78a7599aa106a664f38097c86cc5dc86d
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2026-02-05 23:33:10 +0530

    Add Tooltip component for hover text and button

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
+import Tooltip from "./Tooltip";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <h2 className="top">Hover over the text below:</h2>
+      
+      <Tooltip text="This is a tooltip!">
+        <p>Hover over me to see the magic.</p>
+      </Tooltip>
+
+      <br />
+
+      <Tooltip text="Another tooltip here!">
+        <button>Hover over this button</button>
+      </Tooltip>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
